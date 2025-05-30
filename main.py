9657aa6206039a78eae9883c5ac855780437e8a0
colin sadowitz <sadowitzc@gmail.com>
2025-05-30 13:06:51 -0400

    can receiver setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import can_receiver
+
+
+def main():
+	try:
+		bus = can_receiver.get_data_bus()
+		while True:
+			message = can_receiver.get_can_line(bus)
+			if message == None:
+				break
+	except Exception as e:
+		print ("MAIN::unknown exception:", e)
+
+if __name__ == "__main__":
+	main()
